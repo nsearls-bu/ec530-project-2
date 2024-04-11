39933932525135290e14d487260613d15db63631
Ned Searls <nsearls@bu.edu>
2024-04-11 12:40:43 -0400

    Added some tests for sql injection

--- backend/testing/test_injection.py
+++ backend/testing/test_injection.py
@@ -4,5 +4,19 @@ from io import BytesIO
 from uuid import UUID, uuid4
 import time
 class Test_Analyser(FlaskTests):
+    def test_user_injection(self):
+        data = {}
+        data['file'] = (BytesIO(b"--"), 'test.txt')
+        # Test with SQL comment. This should make the code crash if not properly sanitized.
+        response = self.client.post("/create_document", data=data, content_type='multipart/form-data',)
+        assert response.status_code == 201
 
-    def test_user_uploading(self):
+        response = self.client.post("/create_user", query_string={
+            "username": "; DROP TABLE USERS",
+            "email": "joe@bu.edu",
+            "age": "24",
+            "first_name": "Joe",
+            "last_name": "Smith"
+        })
+        assert response.status_code == 201
+
